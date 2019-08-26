fa7cf44013123376a6972d88cabd927a2c8afed8
Gordon <gordon.nadav@gmail.com>
2019-08-26 14:02:41 +0300

    further api work with post/patch requests for create/edit | navigation for UX purposes + form refactor

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
+import _ from 'lodash';
 
-const StreamEdit = () => {
-    return (
-        <div>
-            StreamEdit
-        </div>
-    )
+class StreamEdit extends Component {
+
+    componentDidMount() {
+        const streamId = this.props.match.params.id;
+        this.props.fetchStream(streamId);
+    }
+    
+    onSubmit = formValues => {
+        const streamId = this.props.match.params.id;
+        this.props.editStream(streamId, formValues);
+    }
+    
+    render() {
+        const currentStream = this.props.stream;
+        if (!currentStream) {
+          return <div>Loading...</div>;
+        }
+        return (
+          <div>
+            <h3>Edit Stream</h3>
+            <StreamForm
+              onSubmit={this.onSubmit}
+              initialValues={_.pick(currentStream, "title", "description")}
+            />
+          </div>
+        );
+    };
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    const streamId = ownProps.match.params.id;
+    return {
+      stream: state.streams[streamId],
+    };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
 
 
